bed5a4b341a895cfd2c6055fc1d17acce852b6a6
Josh McArthur <joshua.mcarthur@gmail.com>
2011-05-31 23:31:31 +1200

    Fixed up deploy script a wee bit

--- script/deploy.rb
+++ script/deploy.rb
@@ -1,17 +1,24 @@
-require 'net/ftp'
-require 'pp'
-
 class FtpDeployer
+
+  require 'net/ftp'
+  require 'yaml'
 
   attr_accessor :host, :username, :password, :remote_dir, :deploy_dir
   attr_accessor :file_permission, :dir_permission
+  attr_accessor :ftp_connection
+  attr_accessor :config
+  def load_config
+    self.config = YAML.load(File.read(File.join(File.dirname(__FILE__), "config.yml")))
+  end
 
-  def initialize(host, username, password = nil)
-    self.host = host
-    self.username = username
+  def initialize(password = nil, environment = :production)
+    load_config
+    puts self.config
+    self.host = self.config[environment.to_s]["host"]
+    self.username = self.config[environment.to_s]["username"]
     self.password = password || ENV['PASSWORD']
-    self.remote_dir = "/www"
-    self.deploy_dir = Dir.pwd
+    self.remote_dir = self.config[environment.to_s]["remote_dir"] || "/www"
+    self.deploy_dir = self.config[environment.to_s]["local_dir"] || Dir.pwd
     self.file_permission = 0644
     self.dir_permission = 0755
   end
@@ -19,43 +26,49 @@ class FtpDeployer
   def deploy
     begin
       puts "Connecting to #{self.host}...\n"
-      ftp =  Net::FTP.new(self.host)
+      self.ftp_connection =  Net::FTP.new(self.host)
       puts "Logging in...\n"
-      ftp.login(self.username, self.password)
-      ftp.chdir(self.remote_dir)
-      Dir.foreach(self.deploy_dir) do |file|
-        if File.directory?(local_file(file))
-          find_or_create_remote_directory(file, ftp)
-          next
-        end
-        puts file
-        puts ftp.mtime(file)
-        ftp.put(remote_file(file)) if File.stat(local_file(file)).mtime > ftp.mtime(remote_file(file))
-      end
-      ftp.close
+      self.ftp_connection.login(self.username, self.password)
+      self.ftp_connection.chdir(self.remote_dir)
+      recursively_upload_from(self.deploy_dir)
+      self.ftp_connection.close
     #rescue Exception => exp
      # puts "ERROR: #{exp.message}\nAborting..."
     end
   end
 
-  def local_file(file)
+  def recursively_upload_from(dir)
+    Dir.foreach(dir) do |file|
+      next if file == "." || file == ".." || file == ".git"
+      if File.directory?(File.join(dir, file))
+        find_or_create_remote_directory(remote_path(File.join(dir, file)))
+        recursively_upload_from(File.join(dir,file))
+      else
+        puts "Uploading #{remote_path(File.join(dir, file))}\n"
+        self.ftp_connection.chdir(remote_path(dir))
+        self.ftp_connection.put(File.join(dir, file), file)
+      end
+    end
+  end
+
+  def local_path(file)
     File.join(self.deploy_dir, file)
   end
 
-  def remote_file(file)
-    self.remote_dir + local_file(file).gsub(self.deploy_dir, '')
+  def remote_path(file)
+    self.remote_dir + local_path(file).gsub(self.deploy_dir, '')
   end
 
   #If we hit an exception, assume the directory does not exist and create it
-  def find_or_create_remote_directory(file, ftp_connection)
+  def find_or_create_remote_directory(file)
     begin
-      return unless File.directory?(local_file(file))
-      ftp_connection.chdir(remote_file(file))
+      return unless File.directory?(local_path(file))
+      self.ftp_connection.chdir(remote_path(file))
     rescue
-      ftp_connection.mkdir(remote_file(file))
+      puts "Creating directory: #{file}"
+      self.ftp_connection.mkdir(remote_path(file))
     end
   end
 end
-
-FtpDeployer.new(ARGV[0], ARGV[1]).deploy
+FtpDeployer.new(ARGV[0]).deploy
 
